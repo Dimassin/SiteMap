4fe4613fbe78665abb9215d762d44e6f2406b918
dima_maximov_git <dmitry.maksimov@izex.ru>
2025-06-29 19:52:51 +0300

    git init

---
+++ src/Exceptions/InvalidFileTypeException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Exceptions;
+class InvalidFileTypeException extends \InvalidArgumentException
+{
+    public function __construct($code = 0, \Throwable $previous = null)
+    {
+        $error = "Unsupported file type. Allowed: xml, json, csv";
+        parent::__construct($error, $code, $previous);
+    }
+}
